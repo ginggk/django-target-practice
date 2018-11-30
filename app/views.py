b1c7e6c0d0c6daeaaf950756e2e72c31201f3501
gingerkeys <gkeys@basecampcodingacademy.org>
2018-11-30 08:40:09 -0600

    achieved 100 percent test coverage again with both and made the both view in views.py.

--- app/views.py
+++ app/views.py
@@ -31,15 +31,52 @@ class Double(View):
 
 class Multiple(View):
     def get(self, request):
-        first_number = request.GET.get('one_number')
-        second_number = request.GET.get('two_number')
-        third_number = request.GET.get('three_number')
+        # if first_number is not None and second_number is not None and third_number is not None:
+        try:
+            first = float(request.GET.get('first_number'))
+            second = float(request.GET.get('second_number'))
+            third = float(request.GET.get('third_number'))
 
-        if first_number is not None and second_number is not None and third_number is not None:
-            first = float(first_number)
-            second = float(second_number)
-            third = float(third_number)
+        except:
+            return render(request, 'app/multiple.html')
+
+        else:
             answer = first * second * third
             return render(request, 'app/multiple.html', {'answer': answer})
+
+
+class Earnings(View):
+    def get(self, request):
+        try:
+            first_number = float(request.GET.get('first_number'))
+            second_number = float(request.GET.get('second_number'))
+            third_number = float(request.GET.get('third_number'))
+        except:
+            return render(request, 'app/earnings.html')
+
         else:
-            return render(request, 'app/multiple.html')
+            first_earnings = first_number * 15
+            second_earnings = second_number * 12
+            third_earnings = third_number * 9
+            total_earnings = first_earnings + second_earnings + third_earnings
+            return render(request, 'app/earnings.html',
+                          {'answer': total_earnings})
+
+
+class Both(View):
+    def get(self, request):
+        first_answer = request.GET.get('first_answer')
+        second_answer = request.GET.get('second_answer')
+        if first_answer.lower() == 'true' and second_answer.lower() == "true":
+            return render(request, 'app/both.html', {'answer': 'True'})
+        elif first_answer.lower() == "false" and second_answer.lower(
+        ) == "false":
+            return render(request, 'app/both.html', {'answer': 'False'})
+        elif first_answer.lower() == "false" and second_answer.lower(
+        ) == "true":
+            return render(request, 'app/both.html', {'answer': 'False'})
+        elif first_answer.lower() == "true" and second_answer.lower(
+        ) == "false":
+            return render(request, 'app/both.html', {'answer': 'False'})
+        else:
+            return render(request, 'app/both.html')
