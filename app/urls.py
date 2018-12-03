86769fbdf36525c093d160181efaca13016f5c65
gingerkeys <gkeys@basecampcodingacademy.org>
2018-12-03 15:59:13 -0600

    added url paths for the rest of the views in views.py

--- app/urls.py
+++ app/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('double/', views.Double.as_view(), name='double'),
     path('multiple/', views.Multiple.as_view(), name='multiple'),
     path('earnings/', views.Earnings.as_view(), name='earnings'),
-    path('both/', views.Both.as_view(), name='both')
+    path('both/', views.Both.as_view(), name='both'),
+    path('walkordrive/', views.walkOrDrive.as_view(), name='walkordrive'),
+    path('populated/', views.HowPopulated.as_view(), name='populated'),
+    path('stars/', views.Stars.as_view(), name='stars'),
+    path('points/', views.Points.as_view(), name='points')
 ]
